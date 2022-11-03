c4e79e1db71bccd5721ff72a5a5e43fd7e3e0e38
Maksim Zinal <mzinal@ru.ibm.com>
2022-11-03 22:35:18 +0300

    правильная обработка изменений в экстракторе

--- dp-compute-colorizer/cf-extract/cfunc.py
+++ dp-compute-colorizer/cf-extract/cfunc.py
@@ -8,15 +8,12 @@ from zoneinfo import ZoneInfo
 import ydb
 
 class WorkContext(object):
-    __slots__ = ("pool", 
-                "dbpath", "table_prefix",
-                "cur_tv")
+    __slots__ = ("pool", "dbpath", "table_prefix")
 
     def __init__(self, pool: ydb.SessionPool, dbpath: str, table_prefix: str) -> None:
         self.pool = pool
         self.dbpath = dbpath
         self.table_prefix = table_prefix
-        self.cur_tv = int(time.time() * 10000000)
 
 def ydbDirExists(driver, path):
     try:
@@ -36,7 +33,7 @@ def tableExists(ctx: WorkContext, tableName: str) -> bool:
         return tableExistsSess(ctx, session, tableName)
     return ctx.pool.retry_operation_sync(callee)
 
-def needExtract(ctx: WorkContext, dt: int):
+def readUpdateLabels(ctx: WorkContext, dt: int):
     def callee(session: ydb.Session):
         qtext = """
             DECLARE $crdate AS Int32;
@@ -45,29 +42,34 @@ def needExtract(ctx: WorkContext, dt: int):
         qp = session.prepare(qtext)
         rs = session.transaction().execute(qp, {"$crdate" : dt})
         if (rs is None or len(rs)==0 or rs[0].rows is None or len(rs[0].rows)==0):
-            upd_tv = 0
+            stamp_prev = 0
         else:
-            upd_tv = rs[0].rows[0].upd_tv
+            stamp_prev = rs[0].rows[0].upd_tv
+        if stamp_prev is None:
+            stamp_prev = 0
         qtext = """
             DECLARE $crdate AS Int32;
-            DECLARE $upd_tv AS Int64;
-            SELECT COUNT(*) AS cnt FROM `{}/item_ref`
-            WHERE crdate=$crdate AND upd_tv >= $upd_tv;
+            SELECT MAX(upd_tv) AS upd_tv FROM `{}/item_ref`
+            WHERE crdate=$crdate;
         """.format(ctx.table_prefix)
         qp = session.prepare(qtext)
-        rs = session.transaction().execute(qp, {"$crdate" : dt, "$upd_tv": upd_tv})
+        rs = session.transaction().execute(qp, {"$crdate" : dt})
         if (rs is None or len(rs)==0 or rs[0].rows is None or len(rs[0].rows)==0):
-            return False
-        return rs[0].rows[0].cnt > 0
+            stamp_cur = 0
+        else:
+            stamp_cur = rs[0].rows[0].upd_tv
+        if stamp_cur is None:
+            stamp_cur = 0
+        return stamp_prev, stamp_cur
     return ctx.pool.retry_operation_sync(callee)
 
-def runExtract(ctx: WorkContext, dt: int):
+def runExtract(ctx: WorkContext, dt: int, upd_tv: int):
     logging.debug("*** Extraction for date {}".format(dt))
     qtext = """
         DECLARE $crdate AS Int32;
         DECLARE $obj_id AS Utf8;
         DECLARE $limit AS Int32;
-        SELECT obj_id, vm_id, cluster_id, otype, upd_tv FROM `{}/item_ref`
+        SELECT obj_id, vm_id, cluster_id, otype FROM `{}/item_ref`
         WHERE crdate=$crdate AND obj_id > $obj_id
         ORDER BY obj_id
         LIMIT $limit;
@@ -89,11 +91,27 @@ def runExtract(ctx: WorkContext, dt: int):
             return False
         for row in rs[0].rows:
             cur_obj_id = row.obj_id
-            logging.debug("{} {} {} {} {}".format(cur_obj_id, row.vm_id, row.cluster_id, row.otype, row.upd_tv))
+            logging.debug("{} {} {} {}".format(cur_obj_id, row.vm_id, row.cluster_id, row.otype))
         return True
     while True:
         if not ctx.pool.retry_operation_sync(callee):
             break
+    # Save the current update timestamp
+    qtext = """
+        DECLARE $crdate AS Int32;
+        DECLARE $upd_tv AS Int64;
+        REPLACE INTO `{}/item_xtr`(crdate, upd_tv) VALUES($crdate, $upd_tv);
+    """.format(ctx.table_prefix)
+    def callee(session: ydb.Session):
+        qp = session.prepare(qtext)
+        session.transaction(ydb.SerializableReadWrite()).execute(
+            qp, 
+            { 
+                "$crdate": dt,
+                "$upd_tv": upd_tv,
+            },
+            commit_tx=True, )
+    ctx.pool.retry_operation_sync(callee)
 
 def runCtx(ctx: WorkContext):
     if not tableExists(ctx, "item_ref"):
@@ -105,12 +123,16 @@ def runCtx(ctx: WorkContext):
     xcur = xtv.date()
     xprev = (xtv - timedelta(days=1)).date()
     # Integer "dates" in YYYYMMDD format
-    cur_date = (xcur.year * 10000) + (xcur.month * 100) + xcur.day
-    prev_date = (xprev.year * 10000) + (xprev.month * 100) + xprev.day
-    if needExtract(ctx, cur_date):
-        runExtract(ctx, cur_date)
-    if needExtract(ctx, prev_date):
-        runExtract(ctx, prev_date)
+    date_now = (xcur.year * 10000) + (xcur.month * 100) + xcur.day
+    date_before = (xprev.year * 10000) + (xprev.month * 100) + xprev.day
+    # Check + extract for day before
+    stamp_prev, stamp_cur = readUpdateLabels(ctx, date_before)
+    if stamp_prev < stamp_cur:
+        runExtract(ctx, date_before, stamp_cur)
+    # Check + extract for today
+    stamp_prev, stamp_cur = readUpdateLabels(ctx, date_now)
+    if stamp_prev < stamp_cur:
+        runExtract(ctx, date_now, stamp_cur)
 
 def run():
     ydb_endpoint = os.getenv("YDB_ENDPOINT")
