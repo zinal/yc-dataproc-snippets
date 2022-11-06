e035bafcd7cb69a76586ec2111d36b1cc6469bc5
Maksim Zinal <mzinal@ru.ibm.com>
2022-11-06 18:23:42 +0300

    отладил установку в виде облачных функций

--- dp-compute-colorizer/install.sh
+++ dp-compute-colorizer/install.sh
@@ -28,7 +28,9 @@ yc_key_key=`cat ${kf} | grep 'secret:' |  (read x y && echo $y)`
 yc lockbox secret create --name ${sa_name} --payload \
   "[{'key': 'ACCESS_KEY_ID', 'text_value': '$yc_key_id'}, {'key': 'SECRET_ACCESS_KEY', 'text_value': '$yc_key_key'}]"
 
-yc lockbox secret add-access-binding --role lockbox.payloadViewer --name ${sa_name} --service-account-name ${sa_name} 
+yc lockbox secret add-access-binding --role lockbox.payloadViewer --name ${sa_name} --service-account-name ${sa_name}
+
+yc_secret_id=`yc lockbox secret get --name ${sa_name} | grep -E '^id: ' | (read x y && echo $y)`
 
 #yc iam key create --service-account-name dp-compute-colorizer --output keys/dp-compute-colorizer.json
 
@@ -44,6 +46,7 @@ yc serverless function version create \
   --environment YDB_ENDPOINT=${ydb_endpoint},YDB_DATABASE=${ydb_database},YDB_PATH=${ydb_path} \
   --source-path cf-scan/dp-compute-scan.zip
 
+# Run each minute
 yc serverless trigger create timer \
   --name ${cf_name}-scan \
   --cron-expression '* * * * ? *' \
@@ -60,11 +63,13 @@ yc serverless function version create \
   --memory 128m \
   --execution-timeout 20s \
   --service-account-id ${sa_id} \
+  --environment YDB_ENDPOINT=${ydb_endpoint},YDB_DATABASE=${ydb_database},YDB_PATH=${ydb_path},S3_BUCKET=${s3_bucket},S3_PREFIX=${s3_prefix},SECRET_ID=${yc_secret_id} \
   --source-path cf-extract/dp-compute-extract.zip
 
+# Run each hour
 yc serverless trigger create timer \
   --name ${cf_name}-xtr \
-  --cron-expression '* * * * ? *' \
+  --cron-expression '0 * ? * * *' \
   --invoke-function-name ${cf_name}-xtr \
   --invoke-function-service-account-id ${sa_id} \
   --retry-attempts 0
