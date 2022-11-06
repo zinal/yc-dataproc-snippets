97a562d9918134b5c5534954e5ed3866afeec94f
Maksim Zinal <mzinal@ru.ibm.com>
2022-11-06 18:34:14 +0300

    поправил скрипты установки/сноса

--- dp-compute-colorizer/cleanup.sh
+++ dp-compute-colorizer/cleanup.sh
@@ -2,12 +2,24 @@
 
 . options.sh
 
+echo `date`" - Deleting trigger ${cf_name}-scan..."
 yc serverless trigger delete --name ${cf_name}-scan
+
+echo `date`" - Deleting function ${cf_name}-scan..."
 yc serverless function delete ${cf_name}-scan
 
+echo `date`" - Deleting trigger ${cf_name}-xtr..."
 yc serverless trigger delete --name ${cf_name}-xtr
+
+echo `date`" - Deleting function ${cf_name}-xtr..."
 yc serverless function delete ${cf_name}-xtr
 
+echo `date`" - Deleting lockbox secret ${sa_name}..."
+yc lockbox secret delete --name ${sa_name}
+
+echo `date`" - Deleting service account ${sa_name}..."
 yc iam service-account delete --name ${sa_name}
+
+echo `date`" - Cleanup completed!"
 
 # End Of File
--- dp-compute-colorizer/install.sh
+++ dp-compute-colorizer/install.sh
@@ -1,5 +1,9 @@
 #! /bin/sh
 
+set -e
+set -u
+
+echo `date`" - Generating ZIP archives for cloud functions..."
 (cd cf-scan && make)
 (cd cf-extract && make)
 
@@ -8,7 +12,11 @@
 # Service account with the required permissions.
 #    We expect that YDB database is created in the same YC folder,
 #    or the permissions need to be configured manually.
+
+echo `date`" - Creating service account ${sa_name}..."
 yc iam service-account create --name ${sa_name}
+
+echo `date`" - Setting permissions for service account ${sa_name}..."
 yc resource-manager folder add-access-binding --role viewer \
    --id ${yc_folder} --service-account-name ${sa_name}
 yc resource-manager folder add-access-binding --role ydb.editor \
@@ -20,22 +28,25 @@ yc resource-manager folder add-access-binding --role storage.uploader \
 
 sa_id=`yc iam service-account get ${sa_name} | grep -E '^id: ' | (read x y && echo $y)`
 
+echo `date`" - Generating static key for service account ${sa_name}..."
 kf=/tmp/${sa_name}_key.txt
 yc iam access-key create --service-account-name ${sa_name} >${kf}
 yc_key_id=`cat ${kf} | grep 'key_id:' |  (read x y && echo $y)`
 yc_key_key=`cat ${kf} | grep 'secret:' |  (read x y && echo $y)`
 
+echo `date`" - Putting the static key into lockbox for service account ${sa_name}..."
 yc lockbox secret create --name ${sa_name} --payload \
   "[{'key': 'ACCESS_KEY_ID', 'text_value': '$yc_key_id'}, {'key': 'SECRET_ACCESS_KEY', 'text_value': '$yc_key_key'}]"
-
+yc_secret_id=`yc lockbox secret get --name ${sa_name} | grep -E '^id: ' | (read x y && echo $y)`
 yc lockbox secret add-access-binding --role lockbox.payloadViewer --name ${sa_name} --service-account-name ${sa_name}
 
-yc_secret_id=`yc lockbox secret get --name ${sa_name} | grep -E '^id: ' | (read x y && echo $y)`
 
 #yc iam key create --service-account-name dp-compute-colorizer --output keys/dp-compute-colorizer.json
 
+echo `date`" - Creating the function ${cf_name}-scan..."
 yc serverless function create --name=${cf_name}-scan
 
+echo `date`" - Creating the function version ${cf_name}-scan..."
 yc serverless function version create \
   --function-name=${cf_name}-scan \
   --runtime python39 \
@@ -47,6 +58,7 @@ yc serverless function version create \
   --source-path cf-scan/dp-compute-scan.zip
 
 # Run each minute
+echo `date`" - Scheduling the function ${cf_name}-scan..."
 yc serverless trigger create timer \
   --name ${cf_name}-scan \
   --cron-expression '* * * * ? *' \
@@ -54,8 +66,10 @@ yc serverless trigger create timer \
   --invoke-function-service-account-id ${sa_id} \
   --retry-attempts 0
 
+echo `date`" - Creating the function ${cf_name}-xtr..."
 yc serverless function create --name=${cf_name}-xtr
 
+echo `date`" - Creating the function version ${cf_name}-xtr..."
 yc serverless function version create \
   --function-name=${cf_name}-xtr \
   --runtime python39 \
@@ -67,11 +81,14 @@ yc serverless function version create \
   --source-path cf-extract/dp-compute-extract.zip
 
 # Run each hour
+echo `date`" - Scheduling the function ${cf_name}-xtr..."
 yc serverless trigger create timer \
   --name ${cf_name}-xtr \
   --cron-expression '0 * ? * * *' \
   --invoke-function-name ${cf_name}-xtr \
   --invoke-function-service-account-id ${sa_id} \
   --retry-attempts 0
+
+echo `date`" - Installation completed!"
 
 # End Of File
