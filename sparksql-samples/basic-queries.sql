8fcfa82b1873988a166967c295a952aba688329c
Maksim Zinal <MaxZinal@yandex.ru>
2022-11-07 13:36:43 +0300

    Update basic-queries.sql

--- sparksql-samples/basic-queries.sql
+++ sparksql-samples/basic-queries.sql
@@ -1,7 +1,6 @@
 -- Data Proc base image 2.1.1 (Spark 3.2.1)
 -- %spark.sql
 
-```SQL
 CREATE TEMPORARY VIEW demo1_tab1m AS (
 WITH tab10(a) AS ((SELECT 0 AS a) UNION ALL (SELECT 1) UNION ALL (SELECT 2) 
                   UNION ALL (SELECT 3) UNION ALL (SELECT 4) UNION ALL (SELECT 5) 
@@ -10,8 +9,5 @@ WITH tab10(a) AS ((SELECT 0 AS a) UNION ALL (SELECT 1) UNION ALL (SELECT 2)
   tab1k(a) AS (SELECT ROW_NUMBER() OVER (ORDER BY x.a, y.a, z.a) 
                FROM tab10 x, tab10 y, tab10 z)
 SELECT ROW_NUMBER() OVER (ORDER BY x.a, y.a) AS num FROM tab1k x, tab1k y);
-```
 
-```SQL
 SELECT MIN(num), MAX(num), AVG(num) FROM demo1_tab1m
-```
