f8660e971690b0f472ef4560483c27cbde78ab47
Maksim Zinal <mzinal@ru.ibm.com>
2022-11-08 09:20:57 +0300

    добавил пропущенные файлы

---
+++ dp-compute-colorizer/cf-extract/pack.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+
+rm -f dp-compute-extract.zip
+zip -9 dp-compute-extract.zip requirements.txt cfunc.py
---
+++ dp-compute-colorizer/cf-scan/pack.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+
+rm -f dp-compute-scan.zip
+zip -9 dp-compute-scan.zip requirements.txt cfunc.py
