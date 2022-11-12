961c3d563057af9234f29b253c56b98acd729892
Maksim Zinal <mzinal@ru.ibm.com>
2022-11-12 16:14:16 +0300

    первоначальный пример скрипта создания кластера Data Proc

---
+++ dataproc-auto/dp-create-cluster.sh
@@ -0,0 +1,44 @@
+#! /bin/sh
+
+# Пример команд для создания кластера Data Proc.
+# Предполагается, что уже созданы
+#  * бакет Object Storage,
+#  * сервисный аккаунт с доступом к бакету,
+#  * внешний сервис Hive Metastore
+
+YC_CLUSTER=dproc1
+YC_VERSION=2.0
+YC_ZONE=ru-central1-b
+YC_SUBNET=default-ru-central1-b
+YC_BUCKET=ms1
+YC_SA=dp1
+YC_METASTORE='thrift://rc1b-dataproc-m-enwlteuuxbltzq8r.mdb.yandexcloud.net:9083'
+
+echo "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBAcV0HrAheoHrN5ow3i6+3QN6TCyB7FNDsQbhK+i8aPNgYsdT4iWDIAllVGxUHRprkBSKSt5tzweXF48pztGTJo= mzinal@ydb1" >ssh-keys.tmp
+
+yc dataproc cluster create ${YC_CLUSTER} \
+  --zone ${YC_ZONE} \
+  --service-account-name ${YC_SA} \
+  --version ${YC_VERSION} --ui-proxy \
+  --services yarn,hive,tez,spark,livy,zeppelin \
+  --bucket ${YC_BUCKET} \
+  --subcluster name="${YC_CLUSTER}_master",role='masternode',resource-preset='s3-c4-m16',disk-type='network-hdd',disk-size=150,hosts-count=1,subnet-name=${YC_SUBNET} \
+  --subcluster name="${YC_CLUSTER}_compute",role='computenode',resource-preset='s3-c4-m16',disk-type='network-ssd-nonreplicated',disk-size=93,hosts-count=1,max-hosts-count=3,subnet-name=${YC_SUBNET} \
+  --ssh-public-keys-file ssh-keys.tmp \
+  --property core:fs.s3a.committer.name=magic \
+  --property core:fs.s3a.committer.magic.enabled=true \
+  --property core:fs.s3a.committer.staging.conflict-mode=fail \
+  --property core:mapreduce.outputcommitter.factory.scheme.s3a=org.apache.hadoop.fs.s3a.commit.S3ACommitterFactory \
+  --property hive:hive.metastore.uris="${YC_METASTORE}" \
+  --property spark:spark.serializer=org.apache.spark.serializer.KryoSerializer \
+  --property spark:spark.kryoserializer.buffer=32m \
+  --property spark:spark.kryoserializer.buffer.max=256m \
+  --property spark:spark.sql.hive.metastore.sharedPrefixes=com.amazonaws,ru.yandex.cloud \
+  --property spark:spark.hadoop.fs.s3a.committer.name=magic \
+  --property spark:spark.sql.sources.commitProtocolClass=org.apache.spark.internal.io.cloud.PathOutputCommitProtocol \
+  --property spark:spark.sql.parquet.output.committer.class=org.apache.spark.internal.io.cloud.BindingParquetOutputCommitter \
+  --property spark:spark.executor.extraJavaOptions='-XX:+UseG1GC' \
+  --property spark:spark.executor.cores=2 \
+  --property spark:spark.executor.memory=4g \
+  --property spark:spark.driver.cores=2 \
+  --property spark:spark.driver.memory=4g
