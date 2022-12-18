484520c7e76ab6fd22d58050ec50a0bf92b9dbef
Maksim Zinal <mzinal@ru.ibm.com>
2022-12-18 20:24:45 +0300

    x

--- dataproc-auto/dp-normal.sh
+++ dataproc-auto/dp-normal.sh
@@ -26,7 +26,7 @@ yc dataproc cluster create ${YC_CLUSTER} \
   --bucket ${YC_BUCKET} \
   --subcluster name="master",role='masternode',resource-preset='s2.medium',disk-type='network-ssd',disk-size=100,hosts-count=1,subnet-name=${YC_SUBNET} \
   --subcluster name="data",role='datanode',resource-preset='s2.xlarge',disk-type='network-ssd-nonreplicated',disk-size=372,hosts-count=1,max-hosts-count=1,subnet-name=${YC_SUBNET} \
-  --subcluster name="compute",role='computenode',resource-preset='s2.xlarge',disk-type='network-ssd-nonreplicated',disk-size=186,hosts-count=2,max-hosts-count=8,subnet-name=${YC_SUBNET},autoscaling-decommission-timeout=3600 \
+  --subcluster name="compute",role='computenode',resource-preset='s2.xlarge',disk-type='network-ssd-nonreplicated',disk-size=186,hosts-count=1,max-hosts-count=10,subnet-name=${YC_SUBNET},autoscaling-decommission-timeout=3600 \
   --ssh-public-keys-file ssh-keys.tmp \
   --property core:fs.s3a.committer.name=directory \
   --property core:fs.s3a.committer.staging.conflict-mode=append \
@@ -44,10 +44,12 @@ yc dataproc cluster create ${YC_CLUSTER} \
   --property spark:spark.serializer=org.apache.spark.serializer.KryoSerializer \
   --property spark:spark.kryoserializer.buffer=32m \
   --property spark:spark.kryoserializer.buffer.max=256m \
-  --property spark:spark.sql.hive.metastore.sharedPrefixes=com.amazonaws,ru.yandex.cloud \
   --property spark:spark.hadoop.fs.s3a.committer.name=directory \
   --property spark:spark.sql.sources.commitProtocolClass=org.apache.spark.internal.io.cloud.PathOutputCommitProtocol \
   --property spark:spark.sql.parquet.output.committer.class=org.apache.spark.internal.io.cloud.BindingParquetOutputCommitter \
+  --property spark:spark.driver.extraJavaOptions='-XX:+UseG1GC' \
   --property spark:spark.executor.extraJavaOptions='-XX:+UseG1GC' \
   --property spark:spark.sql.warehouse.dir=s3a://${YC_BUCKET}/wh \
+  --property spark:spark.sql.hive.metastore.sharedPrefixes=com.amazonaws,ru.yandex.cloud \
+  --property spark:spark.sql.addPartitionInBatch.size=1000 \
   --initialization-action 'uri=s3a://dproc-code/init-scripts/init_normal.sh,args='${YC_BUCKET}
