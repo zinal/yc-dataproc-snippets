9c09f2829aac30e7c7e2d7fd52b5f4cb7a1e5cc8
Maksim Zinal <maxzinal@yandex.ru>
2023-03-29 13:16:26 +0300

    mysql jdbc write in pyspark

--- dataproc-auto/pyspark-sql-demo2.py
+++ dataproc-auto/pyspark-sql-demo2.py
@@ -7,9 +7,9 @@ spark = SparkSession.builder \
 
 spark.catalog.listDatabases()
 
-df = spark.sql("SELECT num, CAST(tv AS VARCHAR(50)) AS tv, a, b, c, d, tv_year, tv_month, tv_day FROM demo1.deltatab0")
+df = spark.sql("SELECT num, CAST(tv AS VARCHAR(50)) AS tv, a, b, c, d, tv_year, tv_month, tv_day FROM demo1.deltatab0 WHERE tv_year=2001 AND tv_month IN (1,2,3,4)")
 df.write.format('jdbc').options(
-  url='jdbc:mysql://rc1a-ugjxgmnpu7vqwkar.mdb.yandexcloud.net:3306/db1?useSSL=true',
+  url='jdbc:mysql://c-c9qun220hulvcq00gtl3.rw.mdb.yandexcloud.net:3306/db1?useSSL=true',
   driver='com.mysql.jdbc.Driver',
   user='user1',
   password='passw0rd',
