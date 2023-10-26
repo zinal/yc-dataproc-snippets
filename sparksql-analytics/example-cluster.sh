f7ce52909c0edce5f02fe57a001baa24424f2f7e
Maksim Zinal <mzinal@ru.ibm.com>
2023-10-26 09:31:15 +0300

    скрипты создания кластера для sql аналитики

---
+++ sparksql-analytics/example-cluster.sh
@@ -0,0 +1,67 @@
+#! /bin/sh
+
+# Пример кластера для SQL-аналитики.
+# Основной путь хранения установлен в каталог в S3.
+# Предполагается, что уже созданы
+#  * бакет Object Storage,
+#  * сервисный аккаунт с доступом к бакету,
+#  * экземпляр Hive Metastore,
+#  * группа безопасности с необходимыми настройками
+
+YC_ZONE=ru-central1-b
+YC_SUBNET=default-ru-central1-b
+YC_BUCKET=mzinal-dproc1
+YC_WH=s3a://mzinal-wh1/warehouse
+YC_SA=mzinal-dp1
+YC_MS_URI='thrift://metastore-1.zonne:9083'
+YC_DDB_LOCKBOX=e6q7eqfqs962m6g9rs62
+YC_DDB_ENDPOINT='https://docapi.serverless.yandexcloud.net/ru-central1/b1gfvslmokutuvt2g019/etnac7v7lqqiflor0sem'
+YC_SG=enpechrqs0fia7vfm2u6
+YC_CLUSTER=sqlan1
+
+echo "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBFDUWfFLBZ/OdeL7geZhiaIeMVY6AsJgFe/xQI2c1NMH2HJPh0GMhxkLGJKv4U5YxCrwrFRZb+dRnlzt7L4T9FM= demo@run0" >ssh-keys.tmp
+
+yc dataproc cluster create ${YC_CLUSTER} \
+  --zone ${YC_ZONE} \
+  --service-account-name ${YC_SA} \
+  --version 2.1 --ui-proxy \
+  --services yarn,spark,livy,zeppelin \
+  --bucket ${YC_BUCKET} \
+  --subcluster name="master",role='masternode',resource-preset='s2.medium',disk-type='network-ssd',disk-size=100,hosts-count=1,subnet-name=${YC_SUBNET} \
+  --subcluster name="static",role='computenode',resource-preset='s3-c16-m64',preemptible=false,disk-type='network-ssd-nonreplicated',disk-size=186,hosts-count=1,max-hosts-count=1,subnet-name=${YC_SUBNET} \
+  --subcluster name="dynamic",role='computenode',resource-preset='s3-c16-m64',preemptible=true,disk-type='network-ssd-nonreplicated',disk-size=186,hosts-count=1,max-hosts-count=5,subnet-name=${YC_SUBNET},autoscaling-decommission-timeout=600,stabilization-duration=600s \
+  --ssh-public-keys-file ssh-keys.tmp \
+  --security-group-ids ${YC_SG} \
+  --property yarn:yarn.node-labels.fs-store.root-dir=file:///hadoop/yarn/node-labels \
+  --property yarn:yarn.node-labels.enabled=true \
+  --property yarn:yarn.node-labels.configuration-type=centralized \
+  --property capacity-scheduler:yarn.scheduler.capacity.maximum-am-resource-percent=1.00 \
+  --property capacity-scheduler:yarn.scheduler.capacity.root.default.accessible-node-labels=SPARKAM \
+  --property capacity-scheduler:yarn.scheduler.capacity.root.accessible-node-labels.SPARKAM.capacity=100 \
+  --property capacity-scheduler:yarn.scheduler.capacity.root.default.accessible-node-labels.SPARKAM.capacity=100 \
+  --property core:fs.s3a.committer.threads=100 \
+  --property core:fs.s3a.connection.maximum=1000 \
+  --property core:mapreduce.outputcommitter.factory.scheme.s3a=org.apache.hadoop.fs.s3a.commit.S3ACommitterFactory \
+  --property spark:spark.sql.catalogImplementation=hive \
+  --property spark:spark.hive.metastore.uris=${YC_MS_URI} \
+  --property spark:spark.sql.warehouse.dir=${YC_WH} \
+  --property spark:spark.serializer=org.apache.spark.serializer.KryoSerializer \
+  --property spark:spark.kryoserializer.buffer=32m \
+  --property spark:spark.kryoserializer.buffer.max=256m \
+  --property spark:spark.driver.extraClassPath=/s3data/jars/yc-delta23-multi-dp21-1.1-fatjar.jar \
+  --property spark:spark.executor.extraClassPath=/s3data/jars/yc-delta23-multi-dp21-1.1-fatjar.jar \
+  --property spark:spark.sql.extensions=io.delta.sql.DeltaSparkSessionExtension \
+  --property spark:spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.YcDeltaCatalog \
+  --property spark:spark.delta.logStore.s3a.impl=ru.yandex.cloud.custom.delta.YcS3YdbLogStore \
+  --property spark:spark.io.delta.storage.S3DynamoDBLogStore.ddb.endpoint=${YC_DDB_ENDPOINT} \
+  --property spark:spark.io.delta.storage.S3DynamoDBLogStore.ddb.lockbox=${YC_DDB_LOCKBOX} \
+  --property spark:spark.databricks.delta.snapshotCache.storageLevel=MEMORY_ONLY_SER_2 \
+  --property spark:spark.sql.hive.metastore.sharedPrefixes=com.amazonaws,ru.yandex.cloud \
+  --property spark:spark.sql.addPartitionInBatch.size=1000 \
+  --property livy:livy.spark.deploy-mode=cluster \
+  --property dataproc:hive.thrift.impl=spark \
+  --initialization-action "uri=s3a://${YC_BUCKET}/init-scripts/init_geesefs.sh,args=[${YC_BUCKET},/s3data]" \
+  --initialization-action "uri=s3a://${YC_BUCKET}/init-scripts/init_nodelabels.sh,args=[static,s3a://${YC_BUCKET}/utils]" \
+  --async
+
+# End Of File
---
+++ sparksql-analytics/init_geesefs.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+BUCKET=$1
+MOUNT_POINT=$2
+
+# Загрузка GeeseFS
+#wget -nv https://github.com/yandex-cloud/geesefs/releases/latest/download/geesefs-linux-amd64 -O /usr/local/sbin/geesefs
+wget -nv https://mycop1.website.yandexcloud.net/utils/geesefs-linux-amd64 -O /usr/local/sbin/geesefs
+chmod a+rwx /usr/local/sbin/geesefs
+mkdir -p "${MOUNT_POINT}"
+
+# Подготовка скрипта, выполняющегося при каждой загрузке
+BOOT_SCRIPT="/var/lib/cloud/scripts/per-boot/80-geesefs-mount.sh"
+echo "#!/bin/bash" >> ${BOOT_SCRIPT}
+echo "/usr/local/sbin/geesefs -o allow_other --iam ${BUCKET} ${MOUNT_POINT}" >> ${BOOT_SCRIPT}
+chmod 755 ${BOOT_SCRIPT}
+
+# Запуск скрипта
+${BOOT_SCRIPT}
+
+# End Of File
