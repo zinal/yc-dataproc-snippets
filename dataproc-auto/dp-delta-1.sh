3ce4fb785867522fa576b40883d7b1be07be1423
Maksim Zinal <zinal@ydb.tech>
2024-01-23 10:53:40 +0300

    актуальный пример создания автомасштабируемого кластера под deltalake

--- dataproc-auto/dp-delta-1.sh
+++ dataproc-auto/dp-delta-1.sh
@@ -8,15 +8,16 @@
 #  * кластер Data Proc с сервисом Hive Metastore
 
 YC_VERSION=2.1
-YC_ZONE=ru-central1-c
-YC_SUBNET=default-ru-central1-c
-YC_BUCKET=dproc-wh
+YC_ZONE=ru-central1-d
+YC_SUBNET=zinal-ru-central1-d
+YC_BUCKET=mzinal-dproc1
+YC_BUCKET_WH=mzinal-wh1
 YC_SA=dp1
-YC_MS_URI='thrift://rc1c-dataproc-m-k4gpw2jv9xjkevlj.mdb.yandexcloud.net:9083'
-YC_DDB_LOCKBOX=e6qr20sbgn3ckpalh54p
-YC_DDB_ENDPOINT=https://docapi.serverless.yandexcloud.net/ru-central1/b1gfvslmokutuvt2g019/etngt3b6eh9qfc80vt54/
+YC_MS_URI='thrift://ms1.zonne:9083'
+YC_DDB_LOCKBOX=e6qbeaqhak245qj9ak2c
+YC_DDB_ENDPOINT='https://docapi.serverless.yandexcloud.net/ru-central1/b1g3o4minpkuh10pd2rj/etnfjib1gmua6mvvgdcl/'
 
-echo "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBKbQbtWaYC/XW5efMnhHr0G+6GEl/pCpUmg9+/DpYXYAdqdB67N1EafbsS6JJiI97B+48vwWMJ0iRQ3Ysihg1jk= demo@gw1" >ssh-keys.tmp
+echo "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBCrnvdmnBQL0klOXlVy3ElRXdkmQbTmNIWyGnVYBS2ygZYrYEMiXXIStCxlrxu+1WXDLTlqGa9AZGyyjTfpP3Jk= demo@gw1" >ssh-keys.tmp
 
 YC_CLUSTER=delta-1
 yc dataproc cluster create ${YC_CLUSTER} \
@@ -25,18 +26,31 @@ yc dataproc cluster create ${YC_CLUSTER} \
   --version ${YC_VERSION} --ui-proxy \
   --services yarn,spark,livy,zeppelin \
   --bucket ${YC_BUCKET} \
-  --subcluster name="master",role='masternode',resource-preset='s2.medium',disk-type='network-ssd',disk-size=100,hosts-count=1,subnet-name=${YC_SUBNET} \
-  --subcluster name="compute",role='computenode',resource-preset='m3-c16-m128',preemptible=true,disk-type='network-ssd-nonreplicated',disk-size=186,hosts-count=3,max-hosts-count=3,subnet-name=${YC_SUBNET},autoscaling-decommission-timeout=3600 \
+  --subcluster name="master",role='masternode',resource-preset='m3-c4-m32',disk-type='network-ssd',disk-size=100,hosts-count=1,subnet-name=${YC_SUBNET} \
+  --subcluster name="static",role='computenode',resource-preset='m3-c16-m128',preemptible=false,disk-type='network-ssd',disk-size=200,hosts-count=1,max-hosts-count=1,subnet-name=${YC_SUBNET},autoscaling-decommission-timeout=3600 \
+  --subcluster name="dynamic",role='computenode',resource-preset='m3-c16-m128',preemptible=true,disk-type='network-ssd-nonreplicated',disk-size=186,hosts-count=1,max-hosts-count=9,subnet-name=${YC_SUBNET},autoscaling-decommission-timeout=3600 \
   --ssh-public-keys-file ssh-keys.tmp \
+  --property yarn:yarn.node-labels.fs-store.root-dir=file:///hadoop/yarn/node-labels \
+  --property yarn:yarn.node-labels.enabled=true \
+  --property yarn:yarn.node-labels.configuration-type=centralized \
+  --property capacity-scheduler:yarn.scheduler.capacity.maximum-am-resource-percent=1.00 \
+  --property capacity-scheduler:yarn.scheduler.capacity.root.default.accessible-node-labels=SPARKAM \
+  --property capacity-scheduler:yarn.scheduler.capacity.root.accessible-node-labels.SPARKAM.capacity=100 \
+  --property capacity-scheduler:yarn.scheduler.capacity.root.default.accessible-node-labels.SPARKAM.capacity=100 \
+  --property capacity-scheduler:yarn.scheduler.capacity.resource-calculator=org.apache.hadoop.yarn.util.resource.DominantResourceCalculator \
+  --property spark:spark.yarn.am.nodeLabelExpression=SPARKAM \
+  --property livy:livy.spark.deploy-mode=cluster \
   --property core:fs.s3a.committer.threads=100 \
   --property core:fs.s3a.connection.maximum=1000 \
   --property core:mapreduce.outputcommitter.factory.scheme.s3a=org.apache.hadoop.fs.s3a.commit.S3ACommitterFactory \
+  --property spark:spark.sql.sources.commitProtocolClass=org.apache.spark.internal.io.cloud.PathOutputCommitProtocol \
+  --property spark:spark.sql.parquet.output.committer.class=org.apache.spark.internal.io.cloud.BindingParquetOutputCommitter \
   --property spark:spark.hadoop.hive.metastore.uris=${YC_MS_URI} \
-  --property spark:spark.sql.warehouse.dir=s3a://${YC_BUCKET}/wh \
+  --property spark:spark.sql.warehouse.dir=s3a://${YC_BUCKET_WH}/warehouse \
   --property spark:spark.serializer=org.apache.spark.serializer.KryoSerializer \
   --property spark:spark.kryoserializer.buffer=32m \
   --property spark:spark.kryoserializer.buffer.max=256m \
-  --property spark:spark.jars=/s3data/jars/yc-delta-multi-dp21-1.0-fatjar.jar \
+  --property spark:spark.jars=/s3data/jars/yc-delta23-multi-dp21-1.1-fatjar.jar \
   --property spark:spark.sql.extensions=io.delta.sql.DeltaSparkSessionExtension \
   --property spark:spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog \
   --property spark:spark.delta.logStore.s3a.impl=ru.yandex.cloud.custom.delta.YcS3YdbLogStore \
@@ -44,5 +58,6 @@ yc dataproc cluster create ${YC_CLUSTER} \
   --property spark:spark.io.delta.storage.S3DynamoDBLogStore.ddb.lockbox=${YC_DDB_LOCKBOX} \
   --property spark:spark.sql.hive.metastore.sharedPrefixes=com.amazonaws,ru.yandex.cloud \
   --property spark:spark.sql.addPartitionInBatch.size=1000 \
-  --initialization-action 'uri=s3a://dproc-code/init-scripts/init_geesefs.sh,args=['${YC_BUCKET}',/s3data]' \
+  --initialization-action 'uri=s3a://'${YC_BUCKET}'/scripts/init_geesefs.sh,args=['${YC_BUCKET}',/s3data]' \
+  --initialization-action 'uri=s3a://'${YC_BUCKET}'/scripts/init_nodelabels.sh,args=static' \
   --async
